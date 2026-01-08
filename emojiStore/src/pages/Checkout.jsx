c4d053804537593ef124a2b86e4930b90e340098
Oscar Rios <oscarrios0101@gmail.com>
2026-01-07 21:41:57 -0600

    feat: update Checkout component; integrate substractFromCart function and enhance cart item display with empty state and improved layout

--- emojiStore/src/pages/Checkout.jsx
+++ emojiStore/src/pages/Checkout.jsx
@@ -1,7 +1,7 @@
 import { useCart } from "../hooks/useCart";
 
 const Checkout = () => {
-  const { cart, addToCart, removeFromCart } = useCart();
+  const { cart, addToCart, removeFromCart, substractFromCart } = useCart();
 
   const cartTotal = cart.reduce(
     (total, item) => total + item.price * item.quantity,
@@ -11,20 +11,21 @@ const Checkout = () => {
   return (
     <div className="pt-10">
       <section className="bg-slate-900 w-[80vw] flex flex-col items-center justify-center text-center text-slate-100 py-3.5 h-[80vh] mx-auto">
+        {cart.length === 0 && <div>EMPTY CART!</div>}
         {cart.map((item) => (
           <div
             key={item.id}
-            className="border border-red-800 w-full flex flex-row justify-between items-center p-2 mb-2"
+            className="grid grid-cols-6 gap-4 border border-red-800 w-full items-center p-2 mb-2"
           >
-            <div className="text-left">{item.name}</div>
+            <div className=" whitespace-normal text-center">{item.name}</div>
             <div>{item.emoji}</div>
             <div>${item.price}</div>
             <div>amount: {item.quantity}</div>
             <div>total: ${item.price * item.quantity}</div>
-            <div className="flex items-center gap-2">
+            <div className="flex items-center justify-center gap-2">
               <button
                 className="px-2 bg-slate-700 rounded hover:bg-slate-600"
-                onClick={() => removeFromCart(item)}
+                onClick={() => substractFromCart(item)}
               >
                 -
               </button>
@@ -35,6 +36,7 @@ const Checkout = () => {
               >
                 +
               </button>
+              <button onClick={() => removeFromCart(item)}>🗑️</button>
             </div>
           </div>
         ))}
